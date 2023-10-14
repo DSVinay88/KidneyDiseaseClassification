af36735cab0b3f5ee984e62d8aa71c5374bf3460
Swamy Vinay <swamy.vinay7889@gmail.com>
2023-10-14 12:23:17 +0530

    requirements, readme, setup updated

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+
 # KidneyDiseaseClassification
+
+## How to run?
+
+### STEPS 00 - Clone the repository
+
+```bash
+https://github.com/DSVinay88/KidneyDiseaseClassification
+```
+
+### STEP 01 - Create a conda environment after opening the repository
+
+```bash
+conda create -n tf_mlflow python=3.8 -y
+```
+
+```bash
+conda activate tf_mlflow
+```
+
+### STEP 02 - Install the required libraries
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "KidneyDiseaseClassification"
+AUTHOR_USER_NAME = "DSVinay88"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "swamy.vinay7889@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
